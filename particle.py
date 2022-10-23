453efe64bb3b8eb2a4ce180f967da5b7fffa1f58
Shreejan <dolaishreejan@gmail.com>
2022-10-23 14:16:06 +0530

    Done

--- particle.py
+++ particle.py
@@ -21,10 +21,14 @@ class particle:
         pygame.display.update()
         self.running = running
 
+        # initialising clock
+        self.clock = pygame.time.Clock()
+
         # colors
         self.blue = (0, 0, 255)
         self.red = (255, 0, 0)
         self.green = (0, 255, 0)
+        self.yellow = (255, 255, 0)
 
     # function to create
     # one particle on the scree
@@ -37,7 +41,6 @@ class particle:
 
     def returnParticle(self, x, y, color, vx, vy):
         return {"x": x, "y": y, "color": color, "vx": vx, "vy": vy}
-
 
     # returns a list consisting
     # of all the necessary things
@@ -53,46 +56,81 @@ class particle:
             group.append(self.returnParticle(x, y, color, vx, vy))
         return group
 
-
     # the function to create
     # many particles
+
     def createParticles(self, particles):
         for particle in particles:
             self.createParticle(
                 particle["x"], particle["y"], particle["color"])
 
-
     # this is the mainrule function
-    # which follows F = GMm/r^2 
+    # which follows F = GMm/r^2
     # and this will move the particles towards each other
+    # and also coordinate geometry = squareroot[(x2-x1)^2 + (y2-y1)^2]
+    # and also F = ma
 
     def mainRule(self, particles1, particles2, g):
-        fx = 0
-        fy = 0
-        a = particles1[0]
-        b = particles2[1]
-        dx = a["x"]+b["x"]
-        dy = a["y"]+b["y"]
-        d = math.sqrt(dx*dx+dy*dy)
-        if (d>0):
-            F= g*1/d
-            fx += (F*dx)
-            fy += (F*dy)
-        a["x"] +=fx
-        a["y"] +=fy
+        # first iterating over all the particles in the particles1
+        for i in range(len(particles1)):
+            # setting the force to be applied
+            # in the x and y coordinates
+            fx = 0
+            fy = 0
 
+            # now iterating over the particles2
+            for j in range(len(particles2)):
+                a = particles1[i]
+                b = particles2[j]
+                dx = a["x"]-b["x"]
+                dy = a["y"]-b["y"]
+                # using coordinate geometry to find the distance between two particles
+                d = math.sqrt(dx*dx+dy*dy)
+                if (d > 0):
+                    F = g*1/d
+                    fx += (F*dx)
+                    fy += (F*dy)
+
+            a["vx"] = (a["vx"] + fx)*0.5
+            a["vy"] = (a["vy"] + fy)*0.5
+            # due to the forc applied
+            # the particles also face acceleration
+            # so setting the velocity using the force formula
+            a["x"] += a["vx"]
+            a["y"] += a["vy"]
+
+            # now reversing the particles
+            # when they hit the wall
+            if (a["x"] <= 0 or a["x"] >= 700):
+                a["vx"] *= -1
+            elif (a["y"] <=0 or a["y"] >= 500):
+                a["vy"] *= -1
+
+        self.screen.fill(0)
 
     # the main loop
+
     def gameLoop(self):
-        self.blueParticles = self.manyParticles(2, self.blue)
-        print(self.blueParticles)
+        # defining particles
+        self.yellowParticles = self.manyParticles(20, self.yellow)
+        self.redParticles = self.manyParticles(20, self.red)
+
         while self.running:
-            print(self.blueParticles[0]["x"])
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            self.createParticles(self.blueParticles)
-            self.mainRule(self.blueParticles, self.blueParticles, 1)
+            
+            # creating the particles
+            self.createParticles(self.yellowParticles)
+            self.createParticles(self.redParticles)
+
+            # defining the rules
+            self.mainRule(self.redParticles, self.redParticles, -1)
+            self.mainRule(self.redParticles, self.yellowParticles, -0.1)
+            self.mainRule(self.redParticles, self.yellowParticles, 1)
+
+
+            self.clock.tick(98)
         pygame.quit()
 
 # the main function
